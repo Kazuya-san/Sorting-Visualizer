4644b6efde57832010bf06737f1519c8157bf5f3
Kazuya-san <kazuyasan3286@gmail.com>
2022-04-19 13:55:23 +0500

    Finalized Sorting Visualizer v2 quicksort

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 let randomize_array = document.getElementById("randomize_array_btn");
 let sort_btn = document.getElementById("sort_btn");
 let bars_container = document.getElementById("bars_container");
+let select_algo = document.getElementById("algo");
+let speed = document.getElementById("speed");
 let minRange = 1;
 let maxRange = 100;
 let numOfBars = 100;
 let heightFactor = 6.5;
+let speedFactor = 100;
 let unsorted_array = new Array(numOfBars);
+
+speed.addEventListener("change", (e) => {
+  speedFactor = parseInt(e.target.value);
+});
+
+let algotouse = "";
+
+select_algo.addEventListener("change", function () {
+  algotouse = select_algo.value;
+});
 
 function randomNum(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -60,15 +73,87 @@ async function bubbleSort(array) {
         bars[j + 1].style.height = array[j + 1] * heightFactor + "px";
         bars[j + 1].style.backgroundColor = "lightgreen";
         //bars[j + 1].innerText = array[j + 1];
-        await sleep(10);
+        await sleep(speedFactor);
       }
     }
-    await sleep(10);
+    await sleep(speedFactor);
   }
   return array;
 }
 
+async function swap(items, leftIndex, rightIndex, bars) {
+  var temp = items[leftIndex];
+  items[leftIndex] = items[rightIndex];
+  items[rightIndex] = temp;
+  bars[leftIndex].style.height = items[leftIndex] * heightFactor + "px";
+  bars[leftIndex].style.backgroundColor = "lightgreen";
+  //bars[leftIndex].innerText = items[leftIndex];
+  bars[rightIndex].style.height = items[rightIndex] * heightFactor + "px";
+  bars[rightIndex].style.backgroundColor = "lightgreen";
+  //bars[rightIndex].innerText = items[rightIndex];
+  await sleep(speedFactor);
+}
+async function partition(items, left, right) {
+  let bars = document.getElementsByClassName("bar");
+  let pivotIndex = Math.floor((right + left) / 2);
+  var pivot = items[pivotIndex]; //middle element
+  bars[pivotIndex].style.backgroundColor = "red";
+
+  for (let i = 0; i < bars.length; i++) {
+    if (i != pivotIndex) {
+      bars[i].style.backgroundColor = "aqua";
+    }
+  }
+
+  (i = left), //left pointer
+    (j = right); //right pointer
+  while (i <= j) {
+    while (items[i] < pivot) {
+      i++;
+    }
+    while (items[j] > pivot) {
+      j--;
+    }
+    if (i <= j) {
+      await swap(items, i, j, bars); //sawpping two elements
+      i++;
+      j--;
+    }
+  }
+  return i;
+}
+
+async function quickSort(items, left, right) {
+  var index;
+  if (items.length > 1) {
+    index = await partition(items, left, right); //index returned from partition
+    if (left < index - 1) {
+      //more elements on the left side of the pivot
+      await quickSort(items, left, index - 1);
+    }
+    if (index < right) {
+      //more elements on the right side of the pivot
+      await quickSort(items, index, right);
+    }
+  }
+  return items;
+}
+
+// sort_btn.addEventListener("click", function () {
+//   let sorted_array = quickSort(unsorted_array, 0, numOfBars - 1);
+//   console.log(sorted_array);
+// });
+
 sort_btn.addEventListener("click", function () {
-  let sorted_array = bubbleSort(unsorted_array);
-  console.log(sorted_array);
+  switch (algotouse) {
+    case "bubble":
+      bubbleSort(unsorted_array);
+      break;
+    case "quick":
+      quickSort(unsorted_array, 0, unsorted_array.length - 1);
+      break;
+    default:
+      bubbleSort(unsorted_array);
+      break;
+  }
 });
